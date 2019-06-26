155210c2525aaae5d6fa97f3314a7a6fdf7a71cf
bolajiayodeji <sirbeejay1@gmail.com>
2019-06-26 06:42:45 +0100

    async robots

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox'
-import { robots } from './robots';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(res => res.json())
+          .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (e) => {
@@ -20,13 +25,17 @@ class App extends Component {
         const filterRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-            <div className="tc">
-                <h1>Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filterRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc pa7">Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1>Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filterRobots} />
+                </div>
+            );
+        }
     }
 }
 
